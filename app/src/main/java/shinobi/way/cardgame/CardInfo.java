5468a7035c2210658542f8ac3b695c6a0d8fc354
Ruslan <rus.babaef@yandex.ru>
2020-06-14 02:04:38 +0300

    1.1

---
+++ app/src/main/java/shinobi/way/cardgame/CardInfo.java
@@ -0,0 +1,37 @@
+package shinobi.way.cardgame;
+
+public class CardInfo {
+
+    public int[] konohacards1 = {
+            0,
+            R.drawable.shu,
+            R.drawable.pashke,
+            R.drawable.akemi,
+            R.drawable.rike,
+            R.drawable.kentaru,
+            R.drawable.hiruko,
+    };
+    public int[] konohacardnames1 = {
+            0,
+            R.id.namecard1,
+            R.id.namecard2,
+            R.id.namecard3,
+            R.id.namecard4,
+            R.id.namecard5,
+            R.id.namecard6,
+    };
+    public int[] ivacardnames1 = {
+            0,
+    };
+
+    public int[] ivacards1 = {
+            0,
+            R.drawable.keiko,
+            R.drawable.akito,
+            R.drawable.ryu,
+            R.drawable.kitsu,
+            R.drawable.benkei,
+            R.drawable.teeru,
+    };
+
+}
